3fb72104a007a2c4ae5de66dfc53d4b23afc9f0e
fatih.dogmus <fatih.dogmus@tubitak.gov.tr>
2021-06-01 18:03:58 +0300

    added the ngneat example with builder

--- src/app/form/ngneat-reactive/ngneat-reactive.component.ts
+++ src/app/form/ngneat-reactive/ngneat-reactive.component.ts
@@ -1,35 +1,51 @@
 import { Component } from "@angular/core";
-import { FormControl, FormGroup } from "@ngneat/reactive-forms";
+import { FormControl, FormGroup, FormBuilder } from "@ngneat/reactive-forms";
 import { Validators } from "@angular/forms";
 
-interface RegistrationModel {
+class RegistrationModel {
   username: string;
   password: string;
   gender: "Male" | "Female" | "";
   birthDate: Date;
   age: number;
+
+  constructor(username: string, password: string, gender: "Male" | "Female" | "", birthDate: Date, age: number) {
+    this.username = username;
+    this.password = password;
+    this.gender = gender;
+    this.birthDate = birthDate;
+    this.age = age;
+  }
+
 }
 
 @Component({
-  selector: 'ngneat-reactive',
-  templateUrl: './ngneat-reactive.component.html',
-  styleUrls: ['./ngneat-reactive.component.scss']
+  selector: "ngneat-reactive",
+  templateUrl: "./ngneat-reactive.component.html",
+  styleUrls: ["./ngneat-reactive.component.scss"],
 })
 export class NgneatReactiveComponent {
-
   model: FormGroup<RegistrationModel>;
 
   constructor() {
-    this.model = new FormGroup<RegistrationModel>({
-      username: new FormControl("", [Validators.required, Validators.minLength(4)]),
-      password: new FormControl(""),
-      gender: new FormControl("", [Validators.required]),
-      birthDate: new FormControl(new Date()),
-      age: new FormControl(0)
+    const builder = new FormBuilder();
+    this.model = builder.group<RegistrationModel>({
+      username: "",
+      password: "",
+      gender:"",
+      birthDate: new Date(),
+      age:0
     })
+    // this.model = new FormGroup<RegistrationModel>({
+    //   username: new FormControl<string>("", [Validators.required, Validators.minLength(4)]),
+    //   password: new FormControl<string>(""),
+    //   gender: new FormControl<"Male" | "Female" | "">("", [Validators.required]),
+    //   birthDate: new FormControl<Date>(new Date()),
+    //   age: new FormControl<number>(0),
+    // });
   }
 
   submitted() {
-    console.log(this.model.value)
+    this.model.controls.age.errors$
   }
 }
